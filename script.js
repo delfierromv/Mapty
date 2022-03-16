83d67fa9ebb5d900148a90e93c931bd4f4a5672e
delfierromv <delfierro.mv@gmail.com>
2022-03-16 11:24:25 -0700

    Displaying a map using leaflet library

--- script.js
+++ script.js
@@ -16,12 +16,17 @@ if(navigator.geolocation)
 //the function .getCurrentPosition() accepts two callback functions. 1) THE FIRST one AKA the POSITION PARAMETER will be called when the browser successfully got the coordinates of the current position of the user 2) THE SECOND callback is the Error Callback which is the one that is gonna be called when there happened while getting the coordinates
   navigator.geolocation.getCurrentPosition(function(position){
     // console.log(position);
+    //STEP ONE OF FLOWCHART?
     const {latitude} = position.coords;
     const {longitude} = position.coords;
     // console.log(latitude,longitude);
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    
+    const coords = [latitude, longitude] 
+    //DISPLAYING A MAP USING LEAFLET LIBRARY--STEP 2 OF FLOWCHART?
 
-    const coords = [latitude, longitude]
+    //whenever we use a third-party library, the first thing to do is to basically include it in our site 
+
     //copied from the leaflet website
     const map = L.map('map').setView(coords, 13); // L is the namespace for Leaflet and we have access to it in this script because we have linked the leaflet script before ours and in that script L is basically a global variable inside the script of leaflet and L has a couple methods that we can use.  
     //second parameter is the zoom level, so 10 would be more zoomed out than 13 etc.
@@ -38,7 +43,4 @@ if(navigator.geolocation)
     alert('Could not get your position')
   });
 
-//DISPLAYING A MAP USING LEAFLET LIBRARY
-
-//whenever we use a third-party library, the first thing to do is to basically include it in our site
 
